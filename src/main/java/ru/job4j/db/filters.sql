c65c8755a737717c4d2bc4a08cdc628a13d01f03
Aleksandr Volodin <cursedkun@gmail.com>
2021-12-12 22:47:14 +0300

    3. Фильтры [#1732]

---
+++ src/main/java/ru/job4j/db/filters.sql
@@ -0,0 +1,45 @@
+-- Написать запрос получение всех продуктов с типом "СЫР"
+SELECT product.*
+FROM product
+INNER JOIN type ON product.type_id = type.id
+WHERE type.name = 'СЫР';
+-- Написать запрос получения всех продуктов, у кого в имени есть слово "мороженое"
+SELECT *
+FROM product
+WHERE name LIKE '%мороженое%';
+-- Написать запрос, который выводит все продукты, срок годности которых уже истек
+SELECT *
+FROM product
+WHERE expired_date < CURRENT_DATE;
+-- Написать запрос, который выводит самый дорогой продукт.
+SELECT name, max(price)
+FROM product
+GROUP by name;
+-- Написать запрос, который выводит для каждого типа количество продуктов к нему принадлежащих. В виде имя_типа, количество
+SELECT t.name as имя_типа, COUNT(*) as количество
+FROM type as t
+INNER JOIN product as p on t.id = p.type_id
+GROUP by t.name;
+-- Написать запрос получение всех продуктов с типом "СЫР" и "МОЛОКО"
+SELECT product.*
+FROM product
+INNER JOIN type ON product.type_id = type.id
+WHERE type.name IN ('СЭР', 'МОЛОКО');
+-- Написать запрос, который выводит тип продуктов, которых осталось меньше 10 штук.
+SELECT t.name as имя_типа, COUNT(*) as количество
+FROM type as t
+INNER JOIN product as p on t.id = p.type_id
+GROUP by t.name
+HAVING COUNT(*) < 10;
+-- Вывести все продукты и их тип.
+SELECT p.name as Продукт, t.name as Тип
+FROM product as p
+INNER JOIN type AS t ON p.type_id = t.id;
+
+
+
+
+
+
+
+
